6ccf437e74d2a7d4778223481d2ee47c900033c8
Cheeseless <pedrobettt@gmail.com>
2018-10-14 03:01:17 -0400

    fix

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -9,11 +9,11 @@
     let currentState: config.Scene;
 
     let assetManifest = [
-        { id: "startButton", src: "./Assets/Images/startButton.png" },
-        { id: "restartButton", src: "./Assets/Images/restartButton.png" },
-        { id: "background1", src: "./Assets/Images/background1.jpg" },
-        { id: "background2", src: "./Assets/Images/background2.jpg" },
-        { id: "background3", src: "./Assets/Images/background3.jpg" },
+        { id: "startButton", src: "./Assets/images/startButton.png" },
+        { id: "restartButton", src: "./Assets/images/restartButton.png" },
+        { id: "background1", src: "./Assets/images/background1.jpg" },
+        { id: "background2", src: "./Assets/images/background2.jpg" },
+        { id: "background3", src: "./Assets/images/background3.jpg" },
         { id: "blank", src: "./Assets/images/blank.png" },
         { id: "chicken", src: "./Assets/images/chicken.png" },
         { id: "megaman", src: "./Assets/images/megaman.png" },
